095faaff5be6745ac6a446d3e52165738b7be6f3
Ravikant <raviidhoni@gmail.com>
2022-04-22 20:01:19 +0530

    done

---
+++ index.js
@@ -0,0 +1,79 @@
+console.log("working...")
+// Boolean - (True /False), Strings - (Words, sentence, mix of numbers and words(ex - address)), Number - (integer and decimal values), 2 ,4, 101, 23.50, 34.99 
+
+"2, somelane, somecity - 711101"
+'2, somelane, somecity - 711101'
+"SIdhant", "907654321"
+
+// data types present in JS(Primitive)
+// WHat are Boolean Values - It has only two values True/False
+
+// What are Strings - Anything which is present inside the double quotes or the single  quotes are called as strings
+
+// Numbers - includes Integer and decimal values (2/3/34.66/99.99)
+
+// Variables - They are used to store the values which could be of any type i.e. Number type or String Type or Boolean type.
+
+// rules for assigning names to variables
+// try to make the variables name declarative
+// it uses Camel Case
+// Variable names are case sensitive 
+
+// fName, fname, Fname, fnAme, fnaMe, 
+
+// Keywords are of three types
+// let, var, const
+
+// -----declaraing / initializing/ redeclaring----
+
+// let - These keywords are used to as
+const*******
+
+const lname = "Gupta";
+// lname = "Vinci";
+console.log(lname);
+
+// var - These keywords are used to assign variables with values
+// 1 . They can be declared in a line and then initialized with a value in another lin
+From Sidhant Gupta to Everyone 07:32 PM
+console.log(age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let fname = "sidhant";
+var age = 25;
+// console.log(age);
+
+var age;
+age = 26;
+// console.log(age);
+age = 27;
+console.log(age);
+
+var age = 29;
+console.log(age);
+const lname = "Gupta";
+// lname = "Vinci";
+console.log(lname);
+let fName = "Sidhant";//declared and initialized in the same line
+let fname;
+fname = "Lakhi";
+console.log(fname);
+fname = "Lakhwinder";
+console.log(fname);
